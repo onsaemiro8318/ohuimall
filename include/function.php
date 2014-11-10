2abebd06aacb2dc97a67fab08cd3914ba684f629
kyhfan <yh.kim@minivertising.kr>
2014-11-10 19:02:01 +0900

    트래킹 작�

--- include/function.php
+++ include/function.php
@@ -23,4 +23,15 @@
 
 		return $result;
 	}
+
+	// 유입매체 정보 입력
+	function OM_InsertTrackingInfo($media, $gubun)
+	{
+		global $_gl;
+		global $my_db;
+
+		$query		= "INSERT INTO ".$_gl['tracking_info_table']."(tracking_media, tracking_ipaddr, tracking_date, tracking_gubun) values('".$media."','".$_SERVER['REMOTE_ADDR']."',now(),'".$gubun."')";
+		$result		= mysqli_query($my_db, $query);
+	}
+
 ?>
--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-index.php
+<?
+	include_once "config.php";
+
+
+	unset($media);
+	$media = $_REQUEST[media];
+
+	OM_InsertTrackingInfo($media, $gubun);
+
+	if($check_mobile)
+	{
+		Header("Location:http://www.ohuimall.co.kr/MOBILE/");
+		exit;
+	}else{
+		Header("Location:http://www.ohuimall.co.kr/PC/");
+		exit;
+	}
+
+?>
