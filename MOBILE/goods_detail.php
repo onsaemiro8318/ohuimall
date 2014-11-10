e67f7dbd97c0547d27b6d1e76d3e3b42fdc86900
kyhfan <yh.kim@minivertising.kr>
2014-11-10 16:10:19 +0900

    index파일 설정

---
+++ MOBILE/goods_detail.php
@@ -0,0 +1,19 @@
+<?
+	$goods_idx	= $_REQUEST['goods_idx'];
+?>
+<!doctype html>
+  <html lang="en">
+    <head>
+      <title>Document</title>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0"/>
+      <script type='text/javascript' src='../js/jquery-1.11.0.min.js'></script>
+  </head>
+  <body>
+    
+  </body>
+</html>
+
+
+
