a085a2788ee9f9ca31a24e6bbd9bad206bf03e49
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-11-10 18:53:24 +0900

    mobile index image slider 수정

--- MOBILE/index.php
+++ MOBILE/index.php
@@ -25,17 +25,25 @@
       <div class="youtubebox">
         <iframe id="ytplayer" width="100%" src="<?=$_gl['youtube_url']?>" frameborder="0" allowfullscreen></iframe>
       </div>
-      <div id="owl-demo" class="owl-carousel">
+      <div>
+        <div class="customNavigation">
+          <a class="btn prev" style="float:left; width:5%;"><img src="./images/btn_prev.png" style="width:100%; margin-top:100%;"></a>
+        </div>
+        <div id="owl-demo" class="owl-carousel" style="float:left">
 <?php
 $query 		= "SELECT * FROM ".$_gl['goods_info_table']." ";
 $result 	= mysqli_query($my_db, $query);
 while($data = mysqli_fetch_array($result))
 {
 ?>
-        <div class="item"><img src="<?php echo $data[goods_imgurl]?>" alt="Owl Image"><p><?php echo $data[goods_name]?></p><p><?php echo $data[goods_detail]?></p></div>
+          <div class="item"><img src="<?php echo $data[goods_imgurl]?>" alt="Owl Image"><p><?php echo $data[goods_name]?></p><p><?php echo $data[goods_detail]?></p></div>
 <?php
 }
 ?>
+        </div>
+        <div class="customNavigation">
+          <a class="btn next" style="float:right; width:5%;"><img src="./images/btn_next.png" style="width:100%; margin-top:100%; align:right;"></a>
+        <div>
       </div>
     </div>
   </body>
@@ -47,7 +55,7 @@ while($data = mysqli_fetch_array($result))
     }
     
     #owl-demo {
-        width:30%;
+        width:90%;
     }
     #owl-demo .item img{
         display: block;
@@ -59,12 +67,23 @@ while($data = mysqli_fetch_array($result))
     <script type='text/javascript'>
     // 이미지 슬라이드
     $(document).ready(function() {
+      var owl = $("#owl-demo");
+      
       $("#owl-demo").owlCarousel({
         autoPlay: 3000,
         items : 4,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
+        itemsDesktop : [1000,5], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,3], // betweem 900px and 601px
+        itemsTablet: [600,2], //2 items between 600 and 0
+        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
       });	
+      
+      $(".next").click(function(){
+        owl.trigger('owl.next');
+      })
+      $(".prev").click(function(){
+        owl.trigger('owl.prev');
+      })
     });
   
   
