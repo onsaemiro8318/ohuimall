6e57f866edbd38e8bf8707f90a0bcb6ae282acd8
kyhfan <yh.kim@minivertising.kr>
2014-11-11 11:36:32 +0900

    상품 구매 로직 작업4

--- MOBILE/winner_input.php
+++ MOBILE/winner_input.php
@@ -16,6 +16,16 @@
     <script type='text/javascript' src='../js/om.js'></script>
   </head>
   <body>
-  
+    <h1>'어려지는 마음'을 받기위해</h1>
+    <h1>아래와 같이 정보가 필요합니다.</h1>
+    이름 <input type="text" name="mb_name"><br />
+	휴대폰번호 <input type="text" name="mb_phone"><br />
+	우편번호   <input type="text" name="ziipcode1"> - 
+	<input type="text" name="ziipcode2">
+	<input type="button" value="우편번호 찾기"><br />
+	상세주소   <input type="text" name="addr1"><br />
+	<input type="text" name="addr2"><br />
+	
+	rpd;s
   </body>
 </html>
