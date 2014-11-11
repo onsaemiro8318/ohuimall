a929d5e37efbb7bbb6bcc35b343d40d64c1787db
kyhfan <yh.kim@minivertising.kr>
2014-11-11 16:35:44 +0900

    화면 전환 효과 테스트3

--- MOBILE/header.php
+++ MOBILE/header.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0"/>
     <link href="../lib/owl/owl.carousel.css" rel="stylesheet">
     <link href="../lib/owl/owl.theme.css" rel="stylesheet">
+    <link href="/css/jquery.mobile-1.4.5.min.css" rel="stylesheet">
     <link href="http://cdn.poesis.kr/post/search.css" rel="stylesheet" media="all" />
     <script type='text/javascript' src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
     <script type='text/javascript' src='../js/jquery-1.11.0.min.js'></script>
+    <script type='text/javascript' src='../js/jquery.mobile-1.4.5.min.js'></script>
     <script type='text/javascript' src='../js/om.js'></script>
     <script type="text/javascript" src="http://www.youtube.com/player_api"></script>
     <script type='text/javascript' src="../lib/owl/owl.carousel.js"></script>
@@ -19,6 +21,6 @@
     <div>
       <h3>마음을 파는 쇼핑몰</h3>
       <h1>오늘을 가장 어리게</h1>
-      <h4><a href="index.php">Introduce</a> / <a href="index.php">Mall</a> / <a href="faq.php">FAQ</a></h4>
+      <h4><a href="index.php" data-transition="slide">Introduce</a> / <a href="index.php" data-transition="slide">Mall</a> / <a href="faq.php" data-transition="slide">FAQ</a></h4>
     </div>
     <h1>--------------------------------</h1>
