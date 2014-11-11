a2a377568a2f22b7bef73a56e38dfbe9286b5e6e
kyhfan <yh.kim@minivertising.kr>
2014-11-11 14:56:39 +0900

    헤더 파일 수정

--- MOBILE/header.php
+++ MOBILE/header.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0"/>
     <link href="../lib/owl/owl.carousel.css" rel="stylesheet">
     <link href="../lib/owl/owl.theme.css" rel="stylesheet">
-    <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+    <link href="http://cdn.poesis.kr/post/search.css" rel="stylesheet" media="all" />
+    <script type='text/javascript' src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
     <script type='text/javascript' src='../js/jquery-1.11.0.min.js'></script>
     <script type='text/javascript' src='../js/om.js'></script>
     <script type="text/javascript" src="http://www.youtube.com/player_api"></script>
-    <script src="../lib/owl/owl.carousel.js"></script>
+    <script type='text/javascript' src="../lib/owl/owl.carousel.js"></script>
     <script type='text/javascript' src='../js/kakao.link.js'></script>
-    <script src="http://cdn.poesis.kr/post/search.min.js"></script>
-    <link href="http://cdn.poesis.kr/post/search.css" rel="stylesheet" media="all" />
+    <script type='text/javascript' src="http://cdn.poesis.kr/post/search.min.js"></script>
   </head>
   <body>
     <div>
