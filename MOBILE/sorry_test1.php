69b595b273d3e616217b89faf6a3f9dfe00d5b58
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-11-14 16:36:40 +0900

    fb share test

--- MOBILE/sorry_test1.php
+++ MOBILE/sorry_test1.php
@@ -15,7 +15,6 @@
       <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0"/>
       <meta property="og:image" content="http://ohuimall.co.kr/images/imgtest.jpg" />
       <meta property="og:title" content="오늘을 가장 어리게 - Ohui mind mall">
-      <meta property="og:url" content="http://ohuimall.co.kr/">
       <script type='text/javascript' src='../js/jquery-1.11.0.min.js'></script>
       <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
       <script type='text/javascript' src='../js/om.js'></script>
