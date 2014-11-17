ce2851e518bf9a1b7a42e2eec2e0ac4ecb8f441e
kyhfan <yh.kim@minivertising.kr>
2014-11-17 16:23:06 +0900

    미당첨페이지 퍼블리싱 적용

--- MOBILE/sorry.php
+++ MOBILE/sorry.php
@@ -5,35 +5,57 @@
 
 	$goods_info = OM_GetGoodsInfo($goods_idx);
 ?>
+<?
+	// 설정파일
+	include_once "../config.php";
+
+	$goods_idx	= $_REQUEST['goods_idx'];
+
+	$goods_info = OM_GetGoodsInfo($goods_idx);
+?>
 <!doctype html>
-  <html lang="en">
-    <head>
-      <title>오늘을 가장 어리게 - Ohui mind mall</title>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-      <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0"/>
-      <script type='text/javascript' src='../js/jquery-1.11.0.min.js'></script>
-      <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
-      <script type='text/javascript' src='../js/om.js'></script>
-      
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link rel="shortcut icon" type="image/x-icon" href="./img/icon/favicon.ico" />
+      <title>OHUI MALL</title>
+    <!--[if lt IE 9]><script src="./js/html5shiv.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="./js/html5shiv.js"></script><![endif]-->
+    <link href="css/style_m.css" rel="stylesheet" type="text/css">
+    <script type='text/javascript' src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+    <script type='text/javascript' src='../js/jquery-1.11.0.min.js'></script>
+    <script type='text/javascript' src='../js/om.js'></script>
+    <script type='text/javascript' src='../js/kakao.link.js'></script>
   </head>
-  <body>
-    <div>
-      <h3>당신은 오늘 충분히 어리세요</h3>
-      <h1>어린 마음이 필요하시다면 내일 도전하세요</h1>
-      <h4>'최근 본 마음'을 SNS를 통해 공유해주시면 내일 당첨될 확률이 올라갑니다.</h4>
+
+
+  <body class="pop_bg">
+    <div class="popup big">
+      <div class="btn_close">
+        <a href="index.php"><img src="images/btn_close.jpg" width="26" alt=""/></a>
+      </div>
+      <div class="content fail">
+        <div class="title">
+          <img src="images/pop_title_fail.jpg"/>
+        </div>
+        <div class="f_img">
+          <img src="images/pop_title_fail_img_p_01.jpg" alt=""/>
+        </div>
+        <div class="btn_share clearfix">
+          <div class="btn_fb">
+            <a href="#" onclick="javascript:fb_share('<?=$goods_info['goods_name']?>','<?=$goods_info['goods_detail']?>','<?=$goods_info['goods_imgurl']?>','<?=$gubun?>');"><img src="images/btn_fb.jpg" width="40" alt=""/></a>
+          </div>
+          <div class="btn_kt">
+            <a href="#" onclick="javascript:kt_share('<?=$goods_info['goods_name']?>','<?=$goods_info['goods_detail']?>','<?=$goods_info['goods_imgurl']?>','<?=$gubun?>');"><img src="images/btn_kt.jpg" width="40" alt=""/></a>
+          </div>
+        </div>
+      </div>
     </div>
-    <h1>--------------------------------</h1>
-	<div>
-	<img src="<?=$goods_info['goods_imgurl']?>">
-	</div>
-  <p>공유하기</p>
-	<a href="#" onclick="fb_share('<?=$goods_info['goods_name']?>','<?=$goods_info['goods_detail']?>','<?=$goods_info['goods_imgurl']?>','<?=$gubun?>');">facebook</a>
-	<a href="#" onclick="kt_share('<?=$goods_info['goods_name']?>','<?=$goods_info['goods_detail']?>','<?=$goods_info['goods_imgurl']?>','<?=$gubun?>');">KAKAOTALK</a>
+    <div id="fb-root"></div>
   </body>
 </html>
-
-<div id="fb-root"></div>
-
-
-
