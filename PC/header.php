4ec57bce2e0bad6a31588fd699c685752b4275c5
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-11-19 15:15:23 +0900

    상품 목록 hot 설정

--- PC/header.php
+++ PC/header.php
@@ -1,26 +1,54 @@
+<!doctype html>
 <html>
-  <head>
-    <title>내일을부탁해 - 드림풀 매칭그랜트 캠페인</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0"/>
-    <link href="../lib/owl/owl.carousel.css" rel="stylesheet">
-    <link rel="stylesheet" href="../lib/owl/owl.carousel.min.css">
-    <link rel="stylesheet" href="../lib/owl/owl.theme.default.min.css">
-    <link href="http://cdn.poesis.kr/post/search.css" rel="stylesheet" media="all" />
-    <script type='text/javascript' src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
-    <script type='text/javascript' src='../js/jquery-1.11.0.min.js'></script>
-    <script type='text/javascript' src='../js/om.js'></script>
-    <script type='text/javascript' src='../js/jquery.zoom.js'></script>
-    <script type="text/javascript" src="http://www.youtube.com/player_api"></script>
-    <script type='text/javascript' src="../lib/owl/owl.carousel.min.js"></script>
-    <script type='text/javascript' src='../js/kakao.link.js'></script>
-    <script type='text/javascript' src="http://cdn.poesis.kr/post/search.min.js"></script>
-  </head>
-  <body>
-    <div>
-      <h3>마음을 파는 쇼핑몰</h3>
-      <h1>오늘을 가장 어리게</h1>
-      <h4><a href="index.php">Introduce</a> / <a href="index.php">Mall</a> / <a href="faq.php">FAQ</a></h4>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<link rel="shortcut icon" type="image/x-icon" href="./img/icon/favicon.ico" />
+<title>OHUI MALL</title>
+<!--[if lt IE 9]><script src="./js/html5shiv.js"></script><![endif]-->
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/normalize.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="../lib/owl/owl.carousel.min.css">
+<link rel="stylesheet" href="../lib/owl/owl.theme.default.min.css">
+<link rel="stylesheet" href="../lib/iCheck/skins/all.css">
+<link rel="stylesheet" href="../lib/bxslider/jquery.bxslider.css">
+<link rel="stylesheet" href="../lib/Magnific-Popup/magnific-popup.css"> 
+<link href="http://cdn.poesis.kr/post/search.css" rel="stylesheet" media="all" />
+<!-- <link rel="stylesheet" href="../js/jquery.mobile/jquery.mobile-1.4.5.min.css" /> -->
+<script type='text/javascript' src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+<script type='text/javascript' src='../js/jquery-1.11.0.min.js'></script>
+<script type='text/javascript' src='../js/jquery.animsition.min.js'></script>
+<script type='text/javascript' src='../lib/iCheck/icheck.js'></script>
+<!-- <script type='text/javascript' src='../js/jquery.mobile/jquery.mobile-1.4.5.min.js'></script> -->
+<script type='text/javascript' src='../js/om.js'></script>
+<script type="text/javascript" src="http://www.youtube.com/player_api"></script>
+<script type='text/javascript' src="../lib/owl/owl.carousel.min.js"></script>
+<script type='text/javascript' src='../js/kakao.link.js'></script>
+<script type='text/javascript' src="http://cdn.poesis.kr/post/search.min.js"></script>
+<script type="text/javascript" src="../lib/bxslider/plugins/jquery.fitvids.js"></script>
+<script type="text/javascript" src="../lib/bxslider/jquery.bxslider.js"></script>
+<script type="text/javascript" src="../lib/Magnific-Popup/jquery.magnific-popup.js"></script>
+<!-- <script src="js/vendor/modernizr-2.6.2.min.js"></script> -->
+
+</head>
+
+<body class="page">
+	<div class="header">
+    <div class="logo g_960"><a href="#"><img src="images/logo.png" width="76" height="21" alt=""/></a></div>
+    <div class="title"><a href="#"><img src="images/title.png" width="326" height="100" alt=""/></a></div>
+    <div class="navi">
+      <div class="inner g_960">
+        <ul class="clearfix">
+          <li><a href="#"><img src="images/btn_mall.png" width="47" height="16" alt=""/></a></li>
+          <li><img src="images/navi_bar.png" width="1" height="16" alt=""/></li>
+          <li><a href="#"><img src="images/btn_movie.png" width="51" height="16" alt=""/></a></li>
+          <li><img src="images/navi_bar.png" width="1" height="16" alt=""/></li>
+          <li><a href="#"><img src="images/btn_faq.png" width="33" height="16" alt=""/></a></li>
+        </ul>
+      </div>
     </div>
-    <h1>--------------------------------</h1>
+  </div>
