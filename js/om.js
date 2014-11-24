8205554568266d5b5d5ea291b01ef0d70d6333d8
kyhfan <yh.kim@minivertising.kr>
2014-11-24 15:59:30 +0900

    alert 제거

--- js/om.js
+++ js/om.js
@@ -285,7 +285,6 @@ function input_name(gubun)
 		},
 		url: "../main_exec.php",
 		success: function(response){
-			alert(response);
 			if (response == "Y")
 			{
 				$("#winner_phone").val(phone_val);
