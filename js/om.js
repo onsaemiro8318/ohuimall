4834309ec760b372bc17c4a8315d19081a9115e4
kyhfan <yh.kim@minivertising.kr>
2014-11-24 13:06:58 +0900

    페이스북 공유 줄바꿈 수정

--- js/om.js
+++ js/om.js
@@ -48,13 +48,11 @@ function fb_share(name,detail,imgurl,gubun)
     picture: imgurl,
     caption: 'ohuimall.co.kr',
     //description: job + " - " + job_explain
-    description: "어려지는 쇼핑몰에서 '최근 본 어린마음'
-    
-    지금 오휘의 어려지는 쇼핑몰에서
-    오늘을 가장 어리게 하는
-    마음을 가지세요
-    
-    지금 어려지는 쇼핑몰 마음 구매하기"
+    description: "어려지는 쇼핑몰에서 '최근 본 어린마음'"
+    "지금 오휘의 어려지는 쇼핑몰에서"
+    "오늘을 가장 어리게 하는"
+    "마음을 가지세요"
+    "지금 어려지는 쇼핑몰 마음 구매하기"
   },
     function(response) {
       if (response && response.post_id) {
