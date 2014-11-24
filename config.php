d001e4debc077413a08f9814aa4dae33aae735b2
kyhfan <yh.kim@minivertising.kr>
2014-11-24 11:44:15 +0900

    구글 analytics 추가

--- config.php
+++ config.php
@@ -5,8 +5,9 @@
 	include_once "include/global.php"; 			//변수정보
 	include_once "include/function.php"; 		//함수정보
 	include_once "include/dbi.php"; 			//DB 연결정보
-  include_once "include/page.class.php";
-  
+	include_once "include/page.class.php";
+	include_once "include/analyticstracking.php";
+
 	mysqli_query ($my_db,"set names utf8");
 
 	$mobile_agent = array("Iphone","Ipod","Android","Blackberry","SymbianOS|SCH-M\d+","Opera Mini", "Windows ce", "Nokia", "sony" );
