049271fefc58678fbb82b657321996c41de916e1
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-11-24 18:30:00 +0900

    품절 페이지 추가

---
+++ PC/soldout.php
@@ -0,0 +1,37 @@
+<?
+	$goods_idx	= $_REQUEST['goods_idx'];
+
+	// 설정파일
+	include_once "../config.php";
+  include_once "header.php";
+
+	$goods_info = OM_GetGoodsInfo($goods_idx);
+
+
+?>
+    
+	<div class="content g_960">
+       <div class="product_img">
+       		<!--품절시-->
+         <div class="t_soldout"><img src="images/txt_soldout.png" alt=""/></div>
+   	   	 <div class="img"><img src="images/big_product_1.jpg" alt="" border="0"/></div>
+       </div>
+       <div class="product_detail">
+       	   <div class="product_txt">
+       	   	<img src="images/big_product_1_txt.jpg" alt=""/> 
+           </div>
+           <div class="img add"><img src="images/big_product_1_gift.jpg" alt=""/></div>
+           
+      </div> 
+
+     <div class="product_detail_img">
+        <img src="images/big_product_1_detail.jpg" width="960" height="608" alt=""/>
+     </div>
+     
+    </div>
+
+
+<?
+	include_once "footer.php";
+
+?>
