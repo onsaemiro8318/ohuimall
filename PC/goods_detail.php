08c7eee4bdf4a0ce18fbd2d4d717a944c30ed674
kyhfan <yh.kim@minivertising.kr>
2014-11-25 11:24:24 +0900

    퍼블리싱 수정 적용1

--- PC/goods_detail.php
+++ PC/goods_detail.php
@@ -153,211 +153,185 @@
 	opacity: 0;
 }
 	</style>
-  
-  
     <input type="hidden" name="goods_idx" id="goods_idx" value="<?=$goods_idx?>">
-	<div class="content g_960">
-       <div class="product_img">
-<?php
-	$soldout_query 		= "SELECT goods_selcount FROM ".$_gl['goods_info_table']." WHERE idx = ".$goods_idx." ";
-	$soldout_result 	= mysqli_query($my_db, $soldout_query);
-	$soldout_cnt = mysqli_fetch_array($soldout_result);
-	if($soldout_cnt[goods_selcount] >= 10)	
-	{
-?>         
-       		<!--품절시-->
-         <div class="t_soldout"><img src="images/txt_soldout.png" alt=""/></div>
-<?php
-	}
-?>         
-   	   	 <div class="img"><img src="images/big_product_<?=$goods_idx?>.jpg" alt="" border="0"/></div>
-       </div>
-       <div class="product_detail">
-       	   <div class="product_txt">
-       	   	<img src="images/big_product_<?=$goods_idx?>_txt.jpg" alt=""/> 
-           </div>
-           <div class="img add"><img src="images/big_product_<?=$goods_idx?>_gift.jpg" alt=""/></div>
-           <div class="btn_block clearfix">
-           	   <div class="inner">
-                   <div class="btn_getit">
-<?
-	if($soldout_cnt[goods_selcount] >= 10)	
-	{
-?>
-                        <a href="#" onclick="alert('품절되어 구매하실 수 없습니다.\n다른 상품을 구매해 주세요.');return false;"><img src="images/btn_buy.jpg"/></a>
-<?
-	}else{
-?>
-                        <a href="#input_1" class="popup-with-zoom-anim"><img src="images/btn_buy.jpg"/></a>
-<?
-	}
-?>
-                   </div>
-
-                   <div class="btn_share_inview clearfix">
-                       <div class="txt">
-                         <img src="images/share_txt.jpg" alt=""/> 
-                       </div>
-                      <!-- <div class="btn_kt">
-                          <a href="#" onclick="kt_share('<?=$goods_info['goods_name']?>','<?=$goods_info['goods_detail']?>','<?=$goods_info['goods_imgurl']?>','<?=$gubun?>');"><img src="images/btn_kt.jpg" alt=""/></a>
-                      </div> -->
-                      <div class="btn_fb">
-                          <a href="#" onclick="fb_share('<?=$goods_info['goods_name']?>','<?=$goods_info['goods_detail']?>','<?=$goods_info['goods_imgurl']?>','<?=$gubun?>');"><img src="images/btn_fb.jpg" alt=""/></a>
-                      </div>
-                   </div> 
-               </div>
-           </div> 
-      </div> 
-
-     <div class="product_detail_img">
+    <div class="content g_960">
+      <div class="product_img">
+        <div class="img"><img src="images/big_product_<?=$goods_idx?>.jpg" alt="" border="0"/></div>
+      </div>
+      <div class="product_detail">
+        <div class="product_txt">
+          <img src="images/big_product_<?=$goods_idx?>_txt.jpg" alt=""/> 
+        </div>
+        <div class="btn_block clearfix">
+          <div class="inner">
+            <div class="btn_getit">
+              <a href="#"><img src="images/btn_buy.jpg"/></a>
+            </div>
+            <div class="btn_share_inview clearfix">
+              <div class="txt">
+                <img src="images/share_txt.jpg" alt=""/> 
+              </div>
+              <!-- <div class="btn_kt">
+                <a href="#"><img src="images/btn_kt.jpg" alt=""/></a>
+              </div> -->
+              <div class="btn_fb">
+                <a href="#" onclick="fb_share('<?=$goods_info['goods_name']?>','<?=$goods_info['goods_detail']?>','<?=$goods_info['goods_imgurl']?>','<?=$gubun?>');"><img src="images/btn_fb.jpg" alt=""/></a>
+              </div>
+            </div> 
+          </div>
+        </div> 
+      </div>
+      <div class="product_detail_img">
         <img src="images/big_product_<?=$goods_idx?>_detail.jpg" width="960" height="608" alt=""/>
-     </div>
-     
+      </div>
     </div>
 
-
 <!--  이름, 전화번호 입력 받는 DIV 시작  -->
-	<div class="wrap_pop zoom-anim-dialog mfp-hide" id="input_1">
+    <div class="wrap_pop zoom-anim-dialog mfp-hide" id="input_1">
       <div class="header">
-            <div class="btn_close"><a onclick="javascript:close_div('input_1');" style="cursor:pointer"><img src="images/btn_close.jpg" width="70" height="70" alt=""/></a></div>
+        <div class="btn_close"><a onclick="javascript:close_div('input_1');" style="cursor:pointer"><img src="images/btn_close.jpg" width="70" height="70" alt=""/></a></div>
+      </div>
+      <div class="inner">
+        <div class="content info">
+          <div class="title">
+            <img src="images/title_info.jpg" width="382" height="66" alt=""/>
+          </div>
+          <div class="info_block">
+            <ul>
+              <li class="clearfix">
+                <div class="label"><img src="images/txt_name.jpg" width="38" height="21" alt=""/></div>
+                <div class="input"><input type="text" name="mb_name" id="mb_name" onblur="only_kor(this)"></div>
+              </li>
+              <li class="clearfix">
+                <div class="label"><img src="images/txt_phonenum.jpg" width="99" height="21" alt=""/></div>
+                <div class="input"><input type="text" name="mb_phone" id="mb_phone" onkeyup="only_num(this)"></div>
+              </li>
+              <li class="input_notice">
+                <p>'-'없이 번호만 입력해주세요.</p>
+              </li>
+            </ul>
+          </div>
+          <div class="check_block">
+            <ul>
+              <li class="clearfix">
+                <div class="input"><input type="checkbox" name="agree1" id="agree1"></div>
+                <div class="label"><a href="#agree1_div" class="popup-with-zoom-anim"><img src="images/txt_notice_1.jpg" alt=""/></a></div>
+              </li>
+              <li class="clearfix">
+                <div class="input"><input type="checkbox" name="agree2" id="agree2"></div>
+                <div class="label"><a href="#agree2_div" class="popup-with-zoom-anim"><img src="images/txt_notice_2.jpg" alt=""/></a></div>
+              </li>
+            </ul>
+          </div>
+          <div class="btn_block">
+            <a href="#" onclick="input_name('<?=$gubun?>');return false;"><img src="images/btn_input_01.jpg" width="250" height="59" alt=""/></a>
+          </div>
         </div>
-    	<div class="inner">
-            <div class="content info">
-               <div class="title">
-                <img src="images/title_info.jpg" width="382" height="66" alt=""/>
-               </div>
-               <div class="info_block">
-                <ul>
-                  <li class="clearfix">
-                        <div class="label"><img src="images/txt_name.jpg" width="38" height="21" alt=""/></div>
-                        <div class="input"><input type="text" name="mb_name" id="mb_name" onblur="only_kor(this)"></div>
-                  </li>
-                  <li class="clearfix">
-                        <div class="label"><img src="images/txt_phonenum.jpg" width="99" height="21" alt=""/></div>
-                        <div class="input"><input type="text" name="mb_phone" id="mb_phone" onkeyup="only_num(this)"></div>
-                  </li>
-                  <li class="input_notice">
-                    <p>'-'없이 번호만 입력해주세요.</p>
-                  </li>
-                </ul>
-               </div>
-               <div class="check_block">
-                <ul>
-                  <li class="clearfix">
-                        <div class="input"><input type="checkbox" name="agree1" id="agree1"></div>
-                        <div class="label"><a href="#agree1_div" class="popup-with-zoom-anim"><img src="images/txt_notice_1.jpg" alt=""/></a></div>
-                  </li>
-                  <li class="clearfix">
-                        <div class="input"><input type="checkbox" name="agree2" id="agree2"></div>
-                        <div class="label"><a href="#agree2_div" class="popup-with-zoom-anim"><img src="images/txt_notice_2.jpg" alt=""/></a></div>
-                  </li>
-                </ul>
-               </div>
-               <div class="btn_block">
-               	<a href="#" onclick="input_name('<?=$gubun?>');return false;"><img src="images/btn_input_01.jpg" width="250" height="59" alt=""/></a>
-               </div>
-          </div>
-	  </div>
-    </div><!--end wrap_pop-->
-    
+      </div>
+    </div>
+
     <!--  미당첨 DIV 시작  -->
-  	<div class="wrap_pop zoom-anim-dialog mfp-hide" id="sorry_div">
-        <div class="header">
-              <div class="btn_close"><a href="javascript:magnificPopup.close();"><img src="images/btn_close.jpg" width="70" height="70" alt=""/></a></div>
+    <div class="wrap_pop zoom-anim-dialog mfp-hide" id="sorry_div">
+      <div class="header">
+        <div class="btn_close"><a href="javascript:magnificPopup.close();"><img src="images/btn_close.jpg" width="70" height="70" alt=""/></a></div>
+      </div>
+      <div class="inner">
+        <div class="content info">
+          <div class="title">
+            <img src="images/title_fail.jpg" alt=""/>
           </div>
-      	<div class="inner">
-              <div class="content info">
-                 <div class="title">
-             	   		<img src="images/title_fail.jpg" alt=""/>
-                 </div>
-                 <div class="img_one">
-             	   		<img src="images/fail_product_<?=$goods_idx?>.jpg" alt=""/>
-                 </div>
-                 <div class="share_txt">
-         		   		<img src="images/txt_share.jpg" width="324" height="105" alt=""/>
-                 </div>
-                 <div class="btn_share_block clearfix">
-                 		<div class="fb"><a href="#" onclick="javascript:fb_share('<?=$goods_info['goods_name']?>','<?=$goods_info['goods_detail']?>','<?=$goods_info['goods_imgurl']?>','<?=$gubun?>');"><img src="images/btn_fb.jpg" alt=""/></a></div>
-                      <!-- <div class="kt"><a href="#" onclick="javascript:kt_share('<?=$goods_info['goods_name']?>','<?=$goods_info['goods_detail']?>','<?=$goods_info['goods_imgurl']?>','<?=$gubun?>');"><img src="images/btn_kt.jpg" alt=""/></a></div> -->
-                 </div>
-            </div>
-  	  </div>
-      </div><!--end wrap_pop-->
-      
-      <!--  개인정보 활용동의 DIV 시작  -->
-    	<div class="wrap_pop zoom-anim-dialog mfp-hide" id="agree1_div">
-          <div class="header">
-                <div class="btn_close"><a href="#input_1" class="first-popup-link"><img src="images/btn_close.jpg" width="70" height="70" alt=""/></a></div>
-            </div>
-        	<div class="inner">
-                <div class="content notice">
-                   <div class="title">
-                   		<img src="images/title_notice_01.jpg" alt=""/>
-                   </div>
-                   <div class="txt_block">
-                   	개인정보동의
-                  </div>
-              </div>
-    	  </div>
-        </div><!--end wrap_pop-->
-      <!--  개인정보 취급위탁동의 DIV 시작  -->
-    	<div class="wrap_pop zoom-anim-dialog mfp-hide" id="agree2_div">
-          <div class="header">
-                <div class="btn_close"><a href="#input_1" class="first-popup-link"><img src="images/btn_close.jpg" width="70" height="70" alt=""/></a></div>
-            </div>
-        	<div class="inner">
-                <div class="content notice">
-                   <div class="title">
-                   		<img src="images/title_notice_02.jpg" alt=""/>
-                   </div>
-                   <div class="txt_block">
-                   	개인정보취급위탁
-                  </div>
-              </div>
-    	  </div>
-        </div><!--end wrap_pop-->
-        <!--  당첨자 추가 정보 받는 DIV 시작  -->
-      	<div class="wrap_pop zoom-anim-dialog mfp-hide" id="input_2">
+          <div class="img_one">
+            <img src="images/fail_product_<?=$goods_idx?>.jpg" alt=""/>
+          </div>
+          <div class="share_txt">
+            <img src="images/txt_share.jpg" width="324" height="105" alt=""/>
+          </div>
+          <div class="btn_share_block clearfix">
+            <div class="fb"><a href="#" onclick="javascript:fb_share('<?=$goods_info['goods_name']?>','<?=$goods_info['goods_detail']?>','<?=$goods_info['goods_imgurl']?>','<?=$gubun?>');"><img src="images/btn_fb.jpg" alt=""/></a></div>
+            <!-- <div class="kt"><a href="#" onclick="javascript:kt_share('<?=$goods_info['goods_name']?>','<?=$goods_info['goods_detail']?>','<?=$goods_info['goods_imgurl']?>','<?=$gubun?>');"><img src="images/btn_kt.jpg" alt=""/></a></div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--  개인정보 활용동의 DIV 시작  -->
+    <div class="wrap_pop zoom-anim-dialog mfp-hide" id="agree1_div">
+      <div class="header">
+        <div class="btn_close"><a href="#input_1" class="first-popup-link"><img src="images/btn_close.jpg" width="70" height="70" alt=""/></a></div>
+      </div>
+      <div class="inner">
+        <div class="content notice">
+          <div class="title">
+            <img src="images/title_notice_01.jpg" alt=""/>
+          </div>
+          <div class="txt_block">
+          개인정보동의
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--  개인정보 취급위탁동의 DIV 시작  -->
+    <div class="wrap_pop zoom-anim-dialog mfp-hide" id="agree2_div">
+      <div class="header">
+        <div class="btn_close"><a href="#input_1" class="first-popup-link"><img src="images/btn_close.jpg" width="70" height="70" alt=""/></a></div>
+      </div>
+      <div class="inner">
+        <div class="content notice">
+          <div class="title">
+            <img src="images/title_notice_02.jpg" alt=""/>
+          </div>
+          <div class="txt_block">
+          개인정보취급위탁
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--  당첨자 추가 정보 받는 DIV 시작  -->
+    <div class="wrap_pop zoom-anim-dialog mfp-hide" id="input_2">
       <input type="hidden" name="winner_phone" id="winner_phone">
-            <div class="header">
-                  <div class="btn_close"><a onclick="javascript:close_div('input_2');" style="cursor:pointer"><img src="images/btn_close.jpg" width="70" height="70" alt=""/></a></div>
-              </div>
-          	<div class="inner">
-                  <div class="content address">
-                     <div class="title">
-                      <img src="images/title_address.jpg"  alt=""/>
-                     </div>
-                     <div class="info_block">
-                        <div class="input_one add_num clearfix">
-                              <div class="label"><img src="images/txt_addnum.jpg" width="75" height="21" alt=""/></div>
-                              <div class="input">
-                                  <div class="inner clearfix">
-                                      <div class="in"><input type="text" name="postcode1" id="postcode1" readonly></div>
-                                      <div class="dash">-</div>
-                                      <div class="in"><input type="text" name="postcode2" id="postcode2" readonly></div>
-                                      <div class="btn"><a onclick="show_post();return false;"><img src="images/btn_search.jpg" width="50"/></a></div>
-                                  </div>
-                              </div>
-                        </div>
-                        <div class="input_one detail_num clearfix">
-                              <div class="label"><img src="images/txt_address_detail.jpg" width="75" height="22" alt=""/></div>
-                              <div class="input">
-                                  <p class="first"><input type="text" name="addr1" id="addr1"></p>
-                                  <p><input type="text" name="addr2" id="addr2"></p>
-                              </div>
-                        </div>
-                     </div>
-                     <div class="btn_block">
-                     	<a href="#" onclick="input_address();return false;"><img src="images/btn_input_01.jpg" width="250" height="59" alt=""/></a>
-                     </div>
+      <div class="header">
+        <div class="btn_close"><a onclick="javascript:close_div('input_2');" style="cursor:pointer"><img src="images/btn_close.jpg" width="70" height="70" alt=""/></a></div>
+      </div>
+      <div class="inner">
+        <div class="content address">
+          <div class="title">
+            <img src="images/title_address.jpg"  alt=""/>
+          </div>
+          <div class="info_block">
+            <div class="input_one add_num clearfix">
+              <div class="label"><img src="images/txt_addnum.jpg" width="75" height="21" alt=""/></div>
+              <div class="input">
+                <div class="inner clearfix">
+                  <div class="in"><input type="text" name="postcode1" id="postcode1" readonly></div>
+                  <div class="dash">-</div>
+                  <div class="in"><input type="text" name="postcode2" id="postcode2" readonly></div>
+                  <div class="btn"><a onclick="show_post();return false;"><img src="images/btn_search.jpg" width="50"/></a></div>
                 </div>
-      	  </div>
-          </div><!--end wrap_pop-->
-        <!--  주소검색 DIV 시작  -->
-            <div id="post_div" style="display:none;border:5px solid;position:fixed;width:60%;height:600px;margin-left:20%;top:50%;margin-top:-300px;overflow:hidden;-webkit-overflow-scrolling:touch;z-index:999999999999">
-              <img src="//i1.daumcdn.net/localimg/localimages/07/postcode/320/close.png" id="btnCloseLayer" style="cursor:pointer;position:absolute;right:-3px;top:-3px" onclick="closeDaumPostcode()" alt="닫기 버튼">
+              </div>
             </div>
-        <!--  주소검색 DIV 끝  -->
+            <div class="input_one detail_num clearfix">
+              <div class="label"><img src="images/txt_address_detail.jpg" width="75" height="22" alt=""/></div>
+              <div class="input">
+                <p class="first"><input type="text" name="addr1" id="addr1"></p>
+                <p><input type="text" name="addr2" id="addr2"></p>
+              </div>
+            </div>
+          </div>
+          <div class="btn_block">
+            <a href="#" onclick="input_address();return false;"><img src="images/btn_input_01.jpg" width="250" height="59" alt=""/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  당첨자 추가 정보 받는 DIV 끝  -->
+
+    <!--  주소검색 DIV 시작  -->
+    <div id="post_div" style="display:none;border:5px solid;position:fixed;width:60%;height:600px;margin-left:20%;top:50%;margin-top:-300px;overflow:hidden;-webkit-overflow-scrolling:touch;z-index:999999999999">
+      <img src="//i1.daumcdn.net/localimg/localimages/07/postcode/320/close.png" id="btnCloseLayer" style="cursor:pointer;position:absolute;right:-3px;top:-3px" onclick="closeDaumPostcode()" alt="닫기 버튼">
+    </div>
+    <!--  주소검색 DIV 끝  -->
 
     <div id="fb-root"></div>
 	<script src="http://dmaps.daum.net/map_js_init/postcode.js"></script>
