b2c86f30e735934b34e9247e18850b34dcd4ffc9
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-11-26 16:16:23 +0900

    각 인덱스 페이지 최근 본 상품 배너 추가

--- PC/index2.php
+++ PC/index2.php
@@ -132,8 +132,11 @@
           </ul>
         </div>
         <div class="banner_block">
+          <div>
+            <img src="images/list_title.jpg" alt=""/>
+          </div>
           <div class="one_banner">
-            <a href="#"><img src="images/banner_recent.jpg" alt=""/></a>
+            <a href="http://ohuimall.co.kr/PC/goods_detail<?=$end_view?>.php"><img src="images/banner_recent_<?=$end_view?>.jpg" alt="<?=$goods_array[$end_view]['goods_name']?>" title="<?=$goods_array[$end_view]['goods_name']?>"/></a>
           </div>
           <div class="one_banner">
             <a href="http://www.ohui.co.kr/product/detail.jsp?pid=ACM06720&cid1=2&cid2=E" target="_blank"><img src="images/banner_ohui.jpg" alt=""/></a>
