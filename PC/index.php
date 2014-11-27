8e154e0982d682775cb6ee535570cdb7b247576f
kyhfan <yh.kim@minivertising.kr>
2014-11-27 16:55:52 +0900

    오휘 CF URL 추가

--- PC/index.php
+++ PC/index.php
@@ -38,7 +38,7 @@
             <a href="http://ohuimall.co.kr/PC/goods_detail<?=$end_view?>.php"><img src="<?=$recent_image?>" alt="<?=$goods_array[$end_view]['goods_name']?>" title="<?=$goods_array[$end_view]['goods_name']?>"/></a>
           </div>
           <div class="one_banner">
-            <a href="#"><img src="images/banner_go_tvcf.jpg" alt="TVCF 보러가기" title="TVCF 보러가기"/></a>
+            <a href="http://www.youtube.com/watch?v=m5afhT4OakA" target="_blank"><img src="images/banner_go_tvcf.jpg" alt="TVCF 보러가기" title="TVCF 보러가기"/></a>
           </div>
           <div class="one_banner">
             <a href="http://www.ohui.co.kr/product/detail.jsp?pid=ACM06720&cid1=2&cid2=E" target="_blank"><img src="images/banner_ohui.jpg" alt=""/></a>
