7d3f6a6b3d7fcd15fddbc2c7a2ad36ee7dbe8577
kyhfan <yh.kim@minivertising.kr>
2014-12-01 11:29:30 +0900

    faq 수정

--- PC/faq.php
+++ PC/faq.php
@@ -4,16 +4,6 @@
 ?>
 	<div class="content g_960">
     	 <div class="content">
-          <div class="txt_faq">
-            <p>여성들의 피부 변화를 위해 17년간 노력해온 O HUI가</p>
-            <p>이제, 피부를 넘어 삶 자체에 기분 좋은 변화를 선물하는 제품과</p>
-            <p>캠페인들을 이어가려 합니다.</p>
-            <p>그 첫 번째 캠페인으로 '마음을 파는 가게'를 시작합니다.</p>                                    
-            <p></p>
-            <p></p>            
-            <p class="title1">The Delightful Change</p>
-            <p class="title2">O HUI</p>
-          </div>
          	<p><img src="images/faq.jpg" width="960"></p>
          </div>  
     </div>
