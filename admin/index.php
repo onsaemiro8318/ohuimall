1353978c6f1871ea0039b119f7a97abcad916889
kyhfan <yh.kim@minivertising.kr>
2014-12-04 13:59:52 +0900

    관리자페이지 수정

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>TomorrowsKids - Admin</title>
+    <title>O HUI mall - Admin</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -34,7 +34,7 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-lg-6">
-            <h1 class="page-header">내일을 부탁해 Admin</h1>
+            <h1 class="page-header">O HUI mall Admin</h1>
             <ol class="breadcrumb">
               <form name="login_frm" method="post" action="admin_exec.php">
                 <input type="hidden" name="exec" value="login">
